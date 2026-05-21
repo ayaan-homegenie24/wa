84f849b2b9db700565a8d9a0e48b2c1fa5123de8
ayaan-homegenie24 <ayaan.homegenie@gmail.com>
2026-05-21 16:27:04 +0530

    Refactor imports for consistency in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,9 @@
 
 require("dotenv").config();
 const express = require("express");
-const axios = require("axios");
-const app = express();
+const axios   = require("axios");
+const path    = require("path");
+const app     = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
